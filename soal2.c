c49135c607734737daf853a2f8c07805b425a970
Dimas Kamurapi <32903333+kamurapi@users.noreply.github.com>
2017-11-16 11:42:23 +0700

    sempurnakan

--- soal2.c
+++ soal2.c
@@ -82,7 +82,7 @@ static int xmp_read(const char *path, char *buf, size_t size, off_t offset, stru
 		char newnamefile[100];
 		strcpy(newnamefile,fpath);
 		strcat(newnamefile,".ditandai");
-		rename(fpath,c);
+		rename(fpath,newnamefile);
 		
 		if(strstr(fpath,"rahasia")==NULL)
 		{
