3f9a540712efe714a80ad3e54c34126737de69b8
adis azhar <adisazhar123@gmail.com>
2017-11-16 10:44:05 +0700

    copy file func

--- no3.c
+++ no3.c
@@ -1,13 +1,29 @@
-#define FUSE_USE_VERSION 28
+#define FUSE_USE_VERSION 26
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#ifdef linux
+/* For pread()/pwrite()/utimensat() */
+#define _XOPEN_SOURCE 700
+#endif
+
 #include <fuse.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/stat.h>
 #include <dirent.h>
 #include <errno.h>
 #include <sys/time.h>
-#include <stdlib.h>
+#ifdef HAVE_SETXATTR
+#include <sys/xattr.h>
+#endif
+
+#include<stdlib.h>
+#include <sys/types.h>
 
 static const char *dirpath = "/home/adis/Downloads";
 
@@ -59,43 +75,36 @@ static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 	return 0;
 }
 
-int flag (const char *filename){
-    int len = strlen (filename);
-    char file [150];
-
-    strcpy(file,filename+len-4);
-    if(strcmp(file,".pdf")==0) return 1;
-
-    else if(strcmp(file,".doc")==0) return 1;
-
-
-    else if(strcmp(file,".txt")==0) return 1;
-
-    else return 0;
-}
 
 static int xmp_open(const char *path, struct fuse_file_info *file){
     int res;
-	char fpath[1200], sourcefile[1200], targetfile[1200];
+	char fpath[1200], sourcefile[1200], targetfile[1200], c;
     FILE *source, *target;
     
 
     sprintf(fpath, "%s%s", dirpath, path);
-    source = fopen(sourcefile, "w");
+    source = fopen(sourcefile, "r+");
 
+    sprintf(targetfile, "%s.copy", fpath);
 
-    if (flag(fpath)){
-        system ("zenity --error --text 'Terjadi kesalahan! File berisi konten berbahaya.';echo $?");
-        return 1;
-    }
-    else{
-        res=open(fpath, file->flags);
+    target = fopen (targetfile, "w");
 
-        if (res == -1) return -errno;
-    }
-    close (res);
+    while( ( c = fgetc(source) ) != EOF )
+    fputc(c, target);
+   
+	//system("cp 'sourcefile' 'targetfile'");
+
+    fclose(source);
+    fclose(target);
+
+    res = open(fpath, file->flags);
+    if (res == -1)
+    return -errno;
+   
+    close(res);
     return 0;
-}
+   }
+   
 
 static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
 		    struct fuse_file_info *fi)
@@ -108,7 +117,7 @@ static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
 	}
 	else sprintf(fpath, "%s%s",dirpath,path);
 	int res = 0;
-  int fd = 0 ;
+    int fd = 0 ;
 
 	(void) fi;
 	fd = open(fpath, O_RDONLY);
